edefde11c7cac4ad8c0b177e7599a6461760e1f5
Rohit Naik Kundaikar <rohitnaikkundaikar@Rohits-MacBook-Air.local>
2025-11-22 15:13:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
